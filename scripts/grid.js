d239971d2fc74e71bb7a6f96f22ae5656fd25470
LinuxDaniel <danielrobinson1112@gmail.com>
2020-05-22 11:40:53 +0100

    Everything moved to grid object. initialiseAll() added.

--- scripts/grid.js
+++ scripts/grid.js
@@ -1,35 +1,47 @@
-const container = document.getElementById("gridContainer");
+var grid = {
+    _rows:12,//default
+    _cols:12,//default
+    _mines:40,//default
+    _gridArray:[],//set in initialiseGrid
 
-//The grid is initalised. Each cell is a div with the class="cell".
-function initialiseGrid(rows, cols) {
-    container.style.setProperty('--grid-rows', rows);
-    container.style.setProperty('--grid-cols', cols);
-    for (i = 0; i < (rows * cols); i++) {
-      let cell = document.createElement("div");
-      //cell.innerText = (i);
-      container.appendChild(cell).className = "cell";
-    };
-};
-initialiseGrid(12, 12);
+    //Method initialises the grid with rows and cols as paramenters (or defaults).
+    initialiseGrid: function(rows = this._rows, cols = this._cols) {
+        this._rows = rows;//Sets object properties.
+        this._cols = cols;
+        const container = document.getElementById("gridContainer");
+        container.style.setProperty('--grid-rows', rows);//CSS properties
+        container.style.setProperty('--grid-cols', cols);
+        //Loops through and creates the required number of cells.
+        for (i = 0; i < (rows * cols); i++) {
+          let cell = document.createElement("div");
+          container.appendChild(cell).className = "cell";
+        };
+        //Finished grid node array is saved.
+        this.gridArray = document.querySelectorAll(".cell");
+    },
 
-//Grid is initalised with number of mines put in random positions.
-function initaliseMines(mines){
-    let grid = document.querySelectorAll(".cell");
-    //Iterate through the grid and place mines at random.
-    for (i = 0; i < grid.length; i++){
-        console.log(mines);
-        if((Math.random() < 0.05) && (mines !== 0) && (mines.className !== "cell mine")){
-            grid[i].className = "cell mine";
-            grid[i].innerText = "M";
-            mines--;
+    //Method places the required (or default) amount of mines around randomly.
+    initialiseMines: function(mines = this._mines){
+        this._mines = mines;
+        for (i = 0; i < this.gridArray.length; i++){
+            if((Math.random() < 0.05) && (mines !== 0) && (mines.className !== "cell mine")){
+                this.gridArray[i].className = "cell mine";
+                this.gridArray[i].innerText = "M";
+                mines--;
+            }
         }
-    }
-    //If the entire grid has been iterated and all mines have not been placed, then repeat the function until they have.
-    if(mines !== 0){
-        console.log("Function iterated.")
-        initaliseMines(mines);
+        //If the entire grid has been iterated and all mines have not been placed, then repeat the function until they have.
+        if(mines !== 0){
+            this.initialiseMines(mines);
+        }
+    },
+
+    //Calls both initialiseGrid(,) and initialiseMines().
+    initialiseAll: function(rows, cols, mines){
+        this.initialiseGrid(rows,cols);
+        this.initialiseMines(mines);
+        console.log("Initialised!")
     }
 }
-initaliseMines(60);
 
-document.querySelectorAll(".cell")[16].className = "cell mine";
+grid.initialiseAll();
